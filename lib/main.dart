300f13d197fc733938a637cc7eba12c6df138425
clshu <ccss0630@yahoo.com>
2021-06-12 20:08:46 -0700

    1. import 'dart:math'; 2. Randomize the dice numbers.

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -33,14 +34,14 @@ class _DicePageState extends State<DicePage> {
           Expanded(child: TextButton(
               onPressed: () {
                 setState(() {
-                  leftDiceNumber = 5;
+                  leftDiceNumber = Random().nextInt(6) + 1;
                 });
               },
               child: Image.asset('images/dice$leftDiceNumber.png'))),
           Expanded(child: TextButton(
               onPressed: () {
                 setState(() {
-                  rightDiceNumber = 3;
+                  rightDiceNumber = Random().nextInt(6) + 1;
                 });
               },
               child: Image.asset('images/dice$rightDiceNumber.png'))),
